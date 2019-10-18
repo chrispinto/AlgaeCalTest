e7dd187115b9fe88a33daa1abf8f7db00f729b69
chrispinto <chris.pinto0@gmail.com>
2019-10-18 16:12:42 -0700

    Fixing the play video issue due to overlay image

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
 });
+// Getting the video to play function
+// eslint-disable-next-line no-underscore-dangle
+window._wq = window._wq || [];
+// eslint-disable-next-line no-undef
+_wq.push({
+  id: 'zwflowymel',
+  onReady(video) {
+    document.getElementById('play-button-overlay-zwflowymel').addEventListener('click', () => {
+    // Once video is loaded and overlay image is clicked, remove the overlay image
+      document.getElementById('play-button-overlay-zwflowymel').remove();
+      video.play();
+    });
+  },
+});
