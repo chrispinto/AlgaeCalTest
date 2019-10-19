f219efb22a9e40535916ffc92c06d90bac930f11
chrispinto <chris.pinto0@gmail.com>
2019-10-18 22:10:43 -0700

    Creating new guarantee section with working ajax modal content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 // import {ProductBundles} from "./modules/ProductBundles";
 import './styles/main.scss';
 import loadSVGs from './modules/svg-replace';
@@ -34,4 +35,25 @@ for (let x = 0; x < amounts.length; x++) {
   if (content == '0%') {
     document.querySelector('.percent-off').style.display = 'none';
   }
+}
+
+// Load Guarantee Modal on Click with Ajax content
+document.getElementById('month-modal').addEventListener('click', loadOptions);
+function loadOptions() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://www.algaecal.com/wp-json/acf/v3/options/options', true);
+
+  // eslint-disable-next-line func-names
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const options = JSON.parse(this.responseText);
+      const guarantee = options.acf;
+
+      document.getElementById('guarantee-modal-body').innerHTML = guarantee['7yr_full_copy'];
+
+      // Image being pulled in was larger than modal width, so I added the img-fluid class
+      document.querySelector('.wp-image-62567').classList.add('img-fluid');
+    }
+  };
+  xhr.send();
 }
